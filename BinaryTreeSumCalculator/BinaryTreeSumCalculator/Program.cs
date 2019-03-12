7c2eb16a76dc46d4f4f05600dc7df2121943faab
Karolis Leonas <karolis.leonas@gmail.com>
2019-03-12 10:40:12 +0200

    initial project commit

---
+++ BinaryTreeSumCalculator/BinaryTreeSumCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTreeSumCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
